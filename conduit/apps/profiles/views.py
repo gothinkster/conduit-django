ca2fe08f084a10849e6a195b4d77415213275e54
James Brewer <james@jamesbrewer.io>
2016-05-10 19:01:03 -0700

    Articles complete.

---
+++ conduit/apps/profiles/views.py
@@ -0,0 +1,28 @@
+from rest_framework import status
+from rest_framework.exceptions import NotFound
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from .models import Profile
+from .renderers import ProfileJSONRenderer
+from .serializers import ProfileSerializer
+
+
+class ProfileRetrieveAPIView(RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    queryset = Profile.objects.select_related('user')
+    renderer_classes = (ProfileJSONRenderer,)
+    serializer_class = ProfileSerializer
+
+    def retrieve(self, request, username, *args, **kwargs):
+        # Try to retrieve the requested profile and throw an exception if the
+        # profile could not be found.
+        try:
+            profile = self.queryset.get(user__username=username)
+        except Profile.DoesNotExist:
+            raise NotFound('A profile with this username was not found.')
+
+        serializer = self.serializer_class(profile)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
