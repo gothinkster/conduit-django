c3f656697b86baf129fcb6bfcbd5e93c82af0090
James Brewer <james@jamesbrewer.io>
2016-05-14 22:43:03 -0700

    Name change.

--- conduit/apps/core/exceptions.py
+++ conduit/apps/core/exceptions.py
@@ -3,7 +3,7 @@ from rest_framework.views import exception_handler
 def core_exception_handler(exc, context):
     response = exception_handler(exc, context)
     handlers = {
-        'ValidationError': _handle_validation_error
+        'ValidationError': _handle_generic_error
     }
     exception_class = exc.__class__.__name__
 
@@ -12,7 +12,7 @@ def core_exception_handler(exc, context):
 
     return response
 
-def _handle_validation_error(exc, context, response):
+def _handle_generic_error(exc, context, response):
     response.data = {
         'errors': response.data
     }
