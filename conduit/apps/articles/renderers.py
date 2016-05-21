fbd2e6451346b83efe90e75e548dbd74d17e4843
James Brewer <james@jamesbrewer.io>
2016-05-21 13:42:51 -0700

    Comments complete.

--- conduit/apps/articles/renderers.py
+++ conduit/apps/articles/renderers.py
@@ -4,3 +4,9 @@ from conduit.apps.core.renderers import ConduitJSONRenderer
 class ArticleJSONRenderer(ConduitJSONRenderer):
     object_label = 'article'
     object_label_plural = 'articles'
+
+
+class CommentJSONRenderer(ConduitJSONRenderer):
+    object_label = 'comment'
+    object_label_plural = 'comments'
+    
--- conduit/apps/articles/views.py
+++ conduit/apps/articles/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework import mixins, status, viewsets
+from rest_framework import generics, mixins, status, viewsets
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
-from .models import Article
-from .renderers import ArticleJSONRenderer
-from .serializers import ArticleSerializer
+from .models import Article, Comment
+from .renderers import ArticleJSONRenderer, CommentJSONRenderer
+from .serializers import ArticleSerializer, CommentSerializer
 
 
 class ArticleViewSet(mixins.CreateModelMixin,
@@ -58,3 +58,47 @@ class ArticleViewSet(mixins.CreateModelMixin,
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CommentsListCreateAPIView(generics.ListCreateAPIView):
+    lookup_field = 'article__slug'
+    lookup_url_kwarg = 'article_slug'
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    renderer_classes = (CommentJSONRenderer,)
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        filters = {self.lookup_field: self.kwargs[self.lookup_url_kwarg]}
+
+        return Comment.objects.filter(**filters)
+
+    def create(self, request, article_slug=None):
+        data = request.data.get('comment', {})
+        context = {'author': request.user.profile}
+
+        try:
+            context['article'] = Article.objects.get(slug=article_slug)
+        except Article.DoesNotExist:
+            raise NotFound('An article with this slug does not exist.')
+
+        serializer = self.serializer_class(data=data, context=context)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class CommentsDestroyAPIView(generics.DestroyAPIView):
+    lookup_url_kwarg = 'comment_pk'
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Comment.objects.all()
+
+    def destroy(self, request, article_slug=None, comment_pk=None):
+        try:
+            comment = Comment.objects.get(pk=comment_pk)
+        except Comment.DoesNotExist:
+            raise NotFound('A comment with this ID does not exist.')
+
+        comment.destroy()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
