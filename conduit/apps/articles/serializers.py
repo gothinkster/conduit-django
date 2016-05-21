e6cc896e155cfaa42908f7fdf4a192248fc5837b
James Brewer <james@jamesbrewer.io>
2016-05-21 14:55:08 -0700

    Tagging complete.

--- conduit/apps/articles/serializers.py
+++ conduit/apps/articles/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 
 from conduit.apps.profiles.serializers import ProfileSerializer
 
-from .models import Article, Comment
+from .models import Article, Comment, Tag
+from .relations import TagRelatedField
 
 
 class ArticleSerializer(serializers.ModelSerializer):
@@ -14,6 +15,8 @@ class ArticleSerializer(serializers.ModelSerializer):
     favoritesCount = serializers.SerializerMethodField(
         method_name='get_favorites_count'
     )
+
+    tagList = TagRelatedField(many=True, required=False, source='tags')
 
     createdAt = serializers.SerializerMethodField(method_name='get_created_at')
     updatedAt = serializers.SerializerMethodField(method_name='get_updated_at')
@@ -28,14 +31,66 @@ class ArticleSerializer(serializers.ModelSerializer):
             'favorited',
             'favoritesCount',
             'slug',
+            'tagList',
             'title',
             'updatedAt',
         )
 
     def create(self, validated_data):
         author = self.context.get('author', None)
+        tags = validated_data.pop('tags', [])
 
-        return Article.objects.create(author=author, **validated_data)
+        article = Article.objects.create(author=author, **validated_data)
+
+        for tag in tags:
+            article.tags.add(tag)
+
+        return article
+
+    def get_created_at(self, instance):
+        return instance.created_at.isoformat()
+
+    def get_favorited(self, instance):
+        request = self.context.get('request', None)
+
+        if request is None:
+            return False
+
+        if not request.user.is_authenticated():
+            return False
+
+        return request.user.profile.has_favorited(instance)
+
+    def get_favorites_count(self, instance):
+        return instance.favorited_by.count()
+
+    def get_updated_at(self, instance):
+        return instance.updated_at.isoformat()
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = ProfileSerializer(required=False)
+
+    createdAt = serializers.SerializerMethodField(method_name='get_created_at')
+    updatedAt = serializers.SerializerMethodField(method_name='get_updated_at')
+
+    class Meta:
+        model = Comment
+        fields = (
+            'id',
+            'author',
+            'body',
+            'createdAt',
+            'updatedAt',
+        )
+
+    def create(self, validated_data):
+        article = self.context['article']
+        author = self.context['author']
+
+        return Comment.objects.create(
+            author=author, article=article, **validated_data
+        )
 
     def get_created_at(self, instance):
         return instance.created_at.isoformat()
@@ -87,3 +142,12 @@ class CommentSerializer(serializers.ModelSerializer):
 
     def get_updated_at(self, instance):
         return instance.updated_at.isoformat()
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('tag',)
+
+    def to_representation(self, obj):
+        return obj.tag
