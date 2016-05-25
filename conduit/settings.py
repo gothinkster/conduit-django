903e4f90817f89a23817152a8fc24f884f6bdd71
James Brewer <james@jamesbrewer.io>
2016-05-24 20:12:24 -0700

    Add comments to AUTH_USER_MODEL setting.

--- conduit/settings.py
+++ conduit/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Tell Django about the custom `User` model we created. The string
+# `authentication.User` tells Django we are referring to the `User` model in
+# the `authentication` module. This module is registered above in a setting
+# called `INSTALLED_APPS`.
 AUTH_USER_MODEL = 'authentication.User'
 
 REST_FRAMEWORK = {
